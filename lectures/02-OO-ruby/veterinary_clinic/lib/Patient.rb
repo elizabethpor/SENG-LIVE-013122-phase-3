b2ffa3e760f4ef8820e7e04fb896d5dbf094a4b8
Elizabeth <elizabeth.por@gmail.com>
2022-03-15 15:12:36 -0500

    modified code during lecture

--- lectures/02-OO-ruby/veterinary_clinic/lib/Patient.rb
+++ lectures/02-OO-ruby/veterinary_clinic/lib/Patient.rb
@@ -1,1 +1,29 @@
-# Add necessary Patient Class logic here! (See Activity.md)
+class Patient
+    attr_reader :id
+    attr_accessor :name, :species, :age, :owner, :phone
+
+    @@all = []
+
+    #invoked via .new
+    def initialize(id, name, species, age, owner, phone)
+        @id = id    
+        @name = name
+        @species = species
+        @age = age
+        @owner = owner
+        @phone = phone
+
+        #here self represents each instance of our class Patient
+        @@all << self
+    end
+
+    #here self represents our class Patient
+    def self.all
+        @@all
+    end
+
+    #here self represents each instance of our class Patient
+    def give_name
+        self.name
+    end
+end
